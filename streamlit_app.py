7c4ed94ede6b124a732482988a16292f9cc73cd1
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-10-27 20:35:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import json
 st.title("tutorial 2")
-
+# DATAFRAME
 df = pd.DataFrame(
   np.random.randn(50,20),
   columns=["cols" + str(i) for i in range(20)])
@@ -12,3 +12,7 @@ st.write(df)
 #st.DataFrame(df,height=1000, width= 200)
 
 st.DataFrame(np.random.randn(50,20))
+
+# TABLE:
+# there is not any customization or thing it work on only printing data in the form of table:
+st.table(df)
