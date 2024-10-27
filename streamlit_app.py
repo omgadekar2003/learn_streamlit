a2b900022f8e8ec8e14b20086cc874ea47fa3880
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-10-27 19:26:51 +0530

    Update streamlit_app.py
    lecture2: df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import json
 st.title("tutorial 2")
+
+df = pd.DataFrame(
+  np.random.randn(50,20),
+  columns=["cols" + str(i) for i in range(20)])
+st.write(df)
+
+
