ff2c5802cb5ec0819635670f08b25cff62b368b3
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-10-31 11:52:49 +0530

    Update lecture_7_cam_audio_video.py

--- lecture_7_cam_audio_video.py
+++ lecture_7_cam_audio_video.py
@@ -1,1 +1,22 @@
+############## Lecture 7:streamlit media elements - st.image , st.audio , st.video
+import streamlit as st
+from PIL import Image
+# import cv2
+
+### streamlit media elements - st.image
+img = Image.open("deogiri.png")
+# show image:
+st.image(
+  img,
+  caption = "OG",
+  width =100,
+  channels = "RGB"
+)
+
+
+####### audio file:
+
+st.audio("fein.mp3", start_time = 27)
+###### video file###:
+st.video("VID-20220808-WA0001.mp4")
 
