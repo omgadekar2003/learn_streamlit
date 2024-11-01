f8eefa04046c4561e2610d34a7d87545ec39a042
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 10:27:06 +0530

    Update streamlit_app.py
    st.spinner(buffering): we useed to buffer with time .sleep(5) seconds fo  time

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,18 @@ import time
 
 #### st.progress:
 
-txt = "%completed"
-my_bar = st.progress(0, text = txt)
-for pr in range(100):
-  time.sleep(0.1)
-  my_bar.progress(pr + 1 ,text = txt)
+# txt = "%completed"
+# my_bar = st.progress(0, text = txt)
+# for pr in range(100):
+#   time.sleep(0.1)
+#   my_bar.progress(pr + 1 ,text = txt)
 
+####### Spinner / Buffer used when we are downloading something:
+
+with st.spinner("wait for it....."):
+  time.sleep(5)
+
+st.write("wait over")
 
 
 
