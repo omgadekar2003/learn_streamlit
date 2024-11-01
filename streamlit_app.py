ecb4108ba4320f259d046b5bccd4de816256418f
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 07:55:43 +0530

    Update streamlit_app.py
    added audio and video to click on sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,20 @@
 #############Lecture 8:Layout elements. st.sidebar, st.columns, st.tabs, st.expander, st.container
-
+###########
+# choice = st.sidebar.radio(
+#   label = "GBGC",
+#   options =("home","Code Run Manual/Download","Feedback","Aboout Us") 
+# )
 ########### Sidebar:
 import streamlit as st
 
 choice = st.sidebar.radio(
   label = "GBGC",
-  options =("home","Code Run Manual/Download","Feedback","Aboout Us") 
+  options =("audio","video") 
 )
+if choice =="audio":
+  st.audio("fein.mp3")
+  st.write("playing audio")
+
+elif choice == "video":
+  st.video("VID-20220808-WA0001.mp4")
+  st.write("playing video")
