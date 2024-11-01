9fc1356dfd8ac3d89ce1b4ed4acf01f471f5585a
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 17:54:51 +0530

    Update streamlit_app.py
    add data df2

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ import pandas as pd
 import numpy as np
 df1 = pd.DataFrame(np.random.randn(10,2), columns =["col1","col2"])
 my_table = st.table(df1)
+# addin data:
+df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
+my_table = st.table.add_rows(df2)
 
