c35e8e15aaa87932adebde27036721e69bc389c2
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 18:06:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,15 @@ df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
 #my_chart =st.line_chart(df1)
 ## on new added data 
 #my_chart.add_rows(df2)
+
+
 #### visual on continuous data like share market:
 ##adding 5 Rows after every 5 seconds:
 my_chart =st.line_chart(df1)
 for i in range(5):
-  time.sleep(5)
-  df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
-  my_chart.add_rows(df2)
+    time.sleep(5)
+    df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
+    my_chart.add_rows(df2)
 
 
 
