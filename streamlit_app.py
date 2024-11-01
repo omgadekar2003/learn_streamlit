e8cb1cd027a544d02c3a508e27a97fdd008fb0a5
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 16:46:06 +0530

    Update streamlit_app.py
    st.set_page_config

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,19 @@ import datetime
 
 
 ########### st.form:
-form = st.form("Add Info")
-name = form.text_input("Enter Name")
-age = form.slider("enter Age",min_value = 18,max_value = 100, step =1)
-date = form.date_input("enter DOB",value = datetime.date(2004,1,25))
-submitted = form.form_submit_button("Submit")
-if submitted:
- st.write(name,age,date)
- 
+# form = st.form("Add Info")
+# name = form.text_input("Enter Name")
+# age = form.slider("enter Age",min_value = 18,max_value = 100, step =1)
+# date = form.date_input("enter DOB",value = datetime.date(2004,1,25))
+# submitted = form.form_submit_button("Submit")
+# if submitted:
+#  st.write(name,age,date)
+
+##########st.set_page_config:
+
+st.set_page_config(
+ page_title = "OG APP",
+ layout = "wide"
+)
+st.write(""OmG)
+
