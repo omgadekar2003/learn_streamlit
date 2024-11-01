23ecd583724415e5c31c460df7f74ec7cd6629ad
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 17:59:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 df1 = pd.DataFrame(np.random.randn(10,2), columns =["col1","col2"])
-my_table = st.table(df1)
-# addin data:
-# df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
-# my_table.add_rows(df2)
-st.line_chart(df1)
+#my_table = st.table(df1)
+addin data:
+df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
+#my_table.add_rows(df2)
+#st.line_chart(df1)
+my_chart.add_rows(df2)
