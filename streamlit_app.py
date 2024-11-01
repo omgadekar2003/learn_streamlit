7fb28669207fc204ad6747b478dff26916797bc5
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 09:04:25 +0530

    Update streamlit_app.py
    st.columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,32 @@
 ########### Sidebar:
 import streamlit as st
 
-choice = st.sidebar.radio(
-  label = "GBGC",
-  options =("audio","video") 
-)
-if choice =="audio":
-  st.audio("fein.mp3")
-  st.write("playing audio")
+# choice = st.sidebar.radio(
+#   label = "GBGC",
+#   options =("audio","video") 
+# )
+# if choice =="audio":
+#   st.audio("fein.mp3")
+#   st.write("playing audio")
 
-elif choice == "video":
-  st.video("VID-20220808-WA0001.mp4")
-  st.write("playing video")
+# elif choice == "video":
+#   st.video("VID-20220808-WA0001.mp4")
+#   st.write("playing video")
+
+
+######### st.columns:
+
+col1, col2 =st.columns(2,gap="small")
+col1.audio("fein.mp3")
+col1.write("I'm feining for more")
+col2.video("VID-20220808-WA0001.mp4")
+col2.write("walk")
+
+
+
+
+
+
+
+
+
