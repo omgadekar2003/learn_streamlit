2242d4d45b83d9e1be393f810f14744530d4bb52
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 16:27:23 +0530

    Update streamlit_app.py
    st.stop()

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 #############Lecture 9:  st.stop , st.form , st.set_page_config , st.echo , st.help
-########### st.stop:
 
 import streamlit as st
 import time
+########### st.stop:
 
-
+email = st.text_input("Enter Email")
+if not email:
+ st.warning("Enter your email")
+ st.stop()
+st.success("Go ahead!")
 
  
