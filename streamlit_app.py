345b03f91edb173c4be9ffef7a6e6d82b6d15513
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 10:20:26 +0530

    Update streamlit_app.py
    st.progress

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import time
 #### st.progress:
 
 txt = "%completed"
-my_bar = st.pregress(0, text = txt)
+my_bar = st.progress(0, text = txt)
 for pr in range(100):
   my_bar.progress(pr + 1 ,text = txt)
 
