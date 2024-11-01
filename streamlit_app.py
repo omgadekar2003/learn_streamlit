ad5d0172ff506d88960514be2268ef3f738b69b5
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 10:37:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,18 @@ import time
 
 #####status:
 
-import time
-import streamlit as st
+# with st.status("Downloading data..."):
+#     st.write("Searching for data...")
+#     time.sleep(2)
+#     st.write("Found URL.")
+#     time.sleep(1)
+#     st.write("Downloading data...")
+#     time.sleep(1)
 
-with st.status("Downloading data..."):
-    st.write("Searching for data...")
-    time.sleep(2)
-    st.write("Found URL.")
-    time.sleep(1)
-    st.write("Downloading data...")
-    time.sleep(1)
-
-st.button("Rerun")
+# st.button("Rerun")
 
 
+######## ERROR Message:
+
+st.error("I'm Feining for more")
 
