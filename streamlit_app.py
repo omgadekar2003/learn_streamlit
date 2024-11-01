fc2fead5715a65eaf3932f880cd84d31916a9dff
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 16:41:06 +0530

    Update streamlit_app.py
    st.form()

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,22 @@
 #############Lecture 9:  st.stop , st.form , st.set_page_config , st.echo , st.help
 
 import streamlit as st
-#import time
+import datetime
 ########### st.stop:
 
-email = st.text_input("Enter Email")
-if not email:
- st.warning("Enter your email")
- st.stop()
-st.success("Go ahead!")
+# email = st.text_input("Enter Email")
+# if not email:
+#  st.warning("Enter your email")
+#  st.stop()
+# st.success("Go ahead!")
 
+
+########### st.form:
+form = st.form("Add Info")
+name = form.text_input("Enter Name")
+age = form.slider("enter Age",min_value = 18,max_value = 100, step =1)
+date = form.date_input("enter DOB",value = datetime.date(2004,1,25))
+submitted = form.form_submit_button("Submit")
+if submitted:
+ st.write(name,age,date)
  
