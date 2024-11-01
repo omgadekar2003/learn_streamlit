cc3432291757b843048a1d94e174068f339fc746
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-01 07:51:31 +0530

    Update streamlit_app.py
    st.sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 #############Lecture 8:Layout elements. st.sidebar, st.columns, st.tabs, st.expander, st.container
-###########
+
+########### Sidebar:
+import streamlit as st
+
+choice = st.sidebar.radio(
+  label = "GBGC",
+  options =("home","Code Run Manual/Download","Feedback","Aboout Us") 
+)
