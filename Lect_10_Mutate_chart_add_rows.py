f92e8edc11db1364410c1520392811563f4fd98f
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-02 18:35:42 +0530

    Create Lect_10_Mutate_chart_add_rows.py
    save

---
+++ Lect_10_Mutate_chart_add_rows.py
@@ -0,0 +1,26 @@
+#############Lecture 11 | Mutate charts, add_rows.
+
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+# import time
+# df1 = pd.DataFrame(np.random.randn(10,2), columns =["col1","col2"])
+
+#my_table = st.table(df1)
+##adding data:
+#df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
+#my_table.add_rows(df2)
+#st.line_chart(df1)
+## on start inp[ut data
+#my_chart =st.line_chart(df1)
+## on new added data 
+#my_chart.add_rows(df2)
+
+
+#### visual on continuous data like share market:
+##adding 5 Rows after every 5 seconds:
+# my_chart =st.line_chart(df1)
+# for i in range(10):
+#     time.sleep(5)
+#     df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
+#     my_chart.add_rows(df2)
