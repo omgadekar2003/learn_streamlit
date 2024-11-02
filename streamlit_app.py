4c9fa2427519c07d6e44ad1de20d68799845a7af
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-02 19:45:13 +0530

    Update streamlit_app.py
    13 with CNN with mnist data model train used where loading only 1 time with st.cache_load and with inference use st.cache_data change everytime when run

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,77 +1,15 @@
-#############Lecture 11 | Mutate charts, add_rows.
+############## Lecture 13: Caching - st.cache_data , st.cache_resource ########
 
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-# import time
-# df1 = pd.DataFrame(np.random.randn(10,2), columns =["col1","col2"])
+#skip some part of code ot not run everytime.
+# cache_resource when load the data
+# cache_data use when you're doing other stuff than loading a model
 
-#my_table = st.table(df1)
-##adding data:
-#df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
-#my_table.add_rows(df2)
-#st.line_chart(df1)
-## on start inp[ut data
-#my_chart =st.line_chart(df1)
-## on new added data 
-#my_chart.add_rows(df2)
-
-
-#### visual on continuous data like share market:
-##adding 5 Rows after every 5 seconds:
-# my_chart =st.line_chart(df1)
-# for i in range(10):
-#     time.sleep(5)
-#     df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
-#     my_chart.add_rows(df2)
-
-######### Lecture 12 | Session states in Streamlit:
 import streamlit as st
-
-#Initialize session state:
-#st.write(st.session_state)
-# st.session_state
-# if "key" not in st.session_state:
-#  st.session_state["key"] = 1
-# st.session_state
-
-###Change session state value:
-
-# st.session_state
-# if "key" in st.session_state:
-#  st.session_state["key"]=2
-#  st.session_state
-
-### Delete Session state:
-# st.session_state
-# if "key" in st.session_state:
-#  del st.session_state["key"]
-# st.session_state 
-
-#### Bul Delete:
-# st.session_state["k1"]=10
-# st.session_state["k2"]=20
-
-# st.session_state
-# for k in st.session_state.keys():
-#  del st.session_state[k]
-# st.session_state 
-
-#### input widget as session_state:
-# st.session_state
-# st.text_input("Name", key ="name")
-# st.session_state
-
-####### Callback on session_state :
-def form_callback():
-  st.write(st.session_state["my_slider"])
-  st.write(st.session_state["my_checkbox"])
-
-with st.form(key = "Feedback Form"):
-  slider_input = st.slider("Rate Us", 1, 5, 1, key = "my_slider")
-  checkbox_input = st.checkbox("Like it or Not", key = "my_checkbox")
-  submit_button = st.form_submit_button("submit",on_click = form_callback)
-
-
-
+import numy as np
+import pandas as pd
+import torch
+import time
+from full_model import Net
+@st.cache_resources
+@st.cache_data
 
