a3c3679fc9367745ea9a8eb3a28955c396c3aa9e
OM GADEKAR <134825216+omgadekar2003@users.noreply.github.com>
2024-11-02 15:43:44 +0530

    Update streamlit_app.py
    initialsession state

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 #############Lecture 11 | Mutate charts, add_rows.
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-import time
-df1 = pd.DataFrame(np.random.randn(10,2), columns =["col1","col2"])
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+# import time
+# df1 = pd.DataFrame(np.random.randn(10,2), columns =["col1","col2"])
+
 #my_table = st.table(df1)
 ##adding data:
 #df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
@@ -18,12 +19,17 @@ df1 = pd.DataFrame(np.random.randn(10,2), columns =["col1","col2"])
 
 #### visual on continuous data like share market:
 ##adding 5 Rows after every 5 seconds:
-my_chart =st.line_chart(df1)
-for i in range(10):
-    time.sleep(5)
-    df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
-    my_chart.add_rows(df2)
+# my_chart =st.line_chart(df1)
+# for i in range(10):
+#     time.sleep(5)
+#     df2 = pd.DataFrame(np.random.randn(5,2), columns =["col1","col2"])
+#     my_chart.add_rows(df2)
 
+######### Lecture 12 | Session states in Streamlit:
+
+#Initialize session state:
+st.write(st.session_state)
+ 
 
 
 
